5f5ac1b146c0729f503ad35539478de34041d9bf
Will Witek <wwitek@fordham.edu>
2020-11-09 17:49:18 -0500

    with extra sauce!

--- theButton/theMedium.js
+++ theButton/theMedium.js
@@ -11,6 +11,22 @@ function incClicks(){
     document.getElementById("clicks").innerHTML = numClicks;
 }
 
+var totalTimes = 0;
+var lastTime = 0, newTime = 0, timeDiff = 0;
+function getTimeDiff(){
+    newTime = Date.now();
+    timeDiff = Math.abs(newTime - lastTime);
+    console.log(timeDiff);
+    document.getElementById("time").innerHTML = timeDiff;
+    lastTime = newTime;
+
+    totalTimes = totalTimes + timeDiff;
+}
+
+function avSpeed(){
+    var avTime = totalTimes / numClicks;
+    document.getElementById("average").innerHTML = avTime;
+}
 
 function randLoc(){
     document.getElementById("theButton").style.top = getRandomInt(450);
